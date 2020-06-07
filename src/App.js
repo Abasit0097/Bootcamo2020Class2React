77939bb24016359246be25a47962541c32c5f8c4
Abdul Basit Abbasi <abbasi097@gmail.com>
2020-06-07 10:00:23 +0400

    Update App.js
    Bold My Name

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.<br/><br/><br/>
-          Deployed By Abdul Basit Abbasi
+          <strong>Deployed By Abdul Basit Abbasi</strong>
         </p>
         <a
           className="App-link"
